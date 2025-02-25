08a53b56e5aa019c932bcbaa97a2e8d825eea3e2
pascal1888 <36227438+pascalvenema@users.noreply.github.com>
2025-02-25 12:07:48 +0100

    Clean up code, leave original code as comment

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,17 +1,6 @@
 import os
-
-# import tempfile
-# from functools import reduce
-
-# from tinydb import TinyDB, Query
-
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
-
-# db_dir_path = tempfile.gettempdir()
-# db_file_path = os.path.join(db_dir_path, "students.json")
-# student_db = TinyDB(db_file_path)
 
 mongo_uri = os.environ.get("MONGO_URI")
 client = MongoClient(mongo_uri)
@@ -29,6 +18,7 @@ def add(student=None):
     # res = student_db.search(query)
     # if res:
     #     return "already exists", 409
+
     existing_student = students_collection.find_one(
         {"first_name": student.first_name, "last_name": student.last_name}
     )
@@ -50,6 +40,7 @@ def get_by_id(student_id=None, subject=None):
     # student = student_db.get(doc_id=int(student_id))
     # if not student:
     #     return "not found", 404
+
     try:
         student = students_collection.find_one({"_id": ObjectId(student_id)})
     except Exception:
@@ -60,6 +51,7 @@ def get_by_id(student_id=None, subject=None):
     # student["student_id"] = student_id
     # print(student)
     # return student
+
     student["student_id"] = str(student["_id"])
     del student["_id"]
     print(student)
@@ -72,12 +64,11 @@ def delete(student_id=None):
     #     return "not found", 404
     # student_db.remove(doc_ids=[int(student_id)])
     # return student_id
+
     try:
         result = students_collection.delete_one({"_id": ObjectId(student_id)})
     except Exception:
         return "not found", 404
-
     if result.deleted_count == 0:
         return "not found", 404
-
     return student_id
